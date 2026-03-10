855842175f7e01a72c33d6df8fd69f8e10a73e5c
Ammara Khalil <your-email@example.com>
2026-03-10 15:51:55 +0500

    sheikhu ka bacha jldi chala gya

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fashion/',
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      }
-    }
-  }
+  base: './',
 })
+
